c560e2bd4d87a656592cd86dfb93416e037e59cf
Katie Sparr <sparr.katier@gmail.com>
2024-07-28 10:57:34 -0500

    changed color of account page

--- screens/AccSettings.jsx
+++ screens/AccSettings.jsx
@@ -84,7 +84,7 @@ const styles = StyleSheet.create({
     },
     scrollView: {
         flex: 1,
-        backgroundColor: 'pink',
+        backgroundColor: 'grey',
         alignItems: 'center',
         
         
