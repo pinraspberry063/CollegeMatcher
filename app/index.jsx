3e6aa58430113114137cd260b741c2f49ad8fcff
Katie Sparr <sparr.katier@gmail.com>
2024-09-23 08:41:41 -0500

    Add planet slider animation to home page

--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,23 @@
-import React, {useContext, useEffect} from 'react';
-import {SafeAreaView, StyleSheet, Text, View, Button} from 'react-native';
+import React, {useContext, useEffect, useState} from 'react';
+import {SafeAreaView, StyleSheet, Text, View, Button, Dimensions, Animated, TouchableWithoutFeedback , TouchableOpacity, ImageBackground} from 'react-native';
+
 import Ionicons from '@expo/vector-icons/Ionicons';
 import themeContext from '../theme/themeContext';
 import {EventRegister} from 'react-native-event-listeners';
 import auth from '@react-native-firebase/auth';
 
+import PlanetSwiper from '../components/PlanetSlider.jsx';
 const Index = ({navigation}) => {
   const theme = useContext(themeContext);
+
+
+
   return (
+    <ImageBackground source={require('../assets/galaxy.webp')} style={styles.background}>
     <View style={styles.container}>
       <View style={styles.icon}>
         <Ionicons
-          color={theme.color}
+          color='white'
           raised
           name="settings-outline"
           //type='ionicon'
@@ -23,13 +29,29 @@ const Index = ({navigation}) => {
       </View>
 
       <SafeAreaView style={styles.titleContainer}>
-        <Text style={[styles.title, {color: theme.color}]}>
+        <Text style={[styles.title, {color: 'purple'}]}>
           College Matcher
         </Text>
-        <Text style={[styles.subtitle, {color: theme.color}]}>
+        <Text style={[styles.subtitle, {color: 'white'}]}>
           Let colleges find you today!
         </Text>
       </SafeAreaView>
+        
+        <View style={{flex:1, justifyContent: 'center', alignItems: 'center', marginTop: 100}}>
+           {/* Orange central view */}
+        <View style={{marginTop: -30}} >
+          <View style={styles.orangeCircle} />
+        </View>
+          
+          <PlanetSwiper /> 
+        </View>
+
+        
+      
+      
+
+      
+      
 
       <View style={styles.buttonContainer}>
         <Button
@@ -41,25 +63,24 @@ const Index = ({navigation}) => {
           color="#841584"
           accessibilityLabel="Take the quiz to be matched with colleges automatically"
         />
-        {/* <Button
-          style={[styles.button, { textShadowColor: theme.color }]}
-          onPress={() => { navigation.push('MakkAI'); }}
-          title="MAKK AI"
-          color="#841584"
-          accessibilityLabel="Chat with MAKK AI"
-        /> */}
+      
       </View>
     </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover'
+  },
   container: {
     flex: 1,
   },
   titleContainer: {
     alignItems: 'center',
-    paddingTop: 300,
+    paddingTop: 150,
   },
   title: {
     fontSize: 50,
@@ -74,9 +95,18 @@ const styles = StyleSheet.create({
     right: 0,
     backgroundColor: 'light',
   },
+  orangeCircle: {
+    width: 250,
+    height: 250,
+    borderRadius: 225,
+    backgroundColor: 'orange',
+    position: 'absolute', 
+    alignSelf: 'center' 
+  },
   buttonContainer: {
+    flex: 1,
     alignItems: 'center',
-    paddingTop: 200,
+    paddingTop: 100,
   },
   button: {
     width: '50%',
